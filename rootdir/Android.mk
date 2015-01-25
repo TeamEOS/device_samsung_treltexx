6ec63ae2374f768cca2afe40a54b6b609049f6e7
Christopher N. Hesse <raymanfx@gmail.com>
2015-01-25 17:21:27 +0100

    trelte: Load deferred initcalls
    This calls the init function of the wifi driver.
    
    Taken from:
    http://git.cryptomilk.org/projects/android/android_device_samsung_slte.git/commit/ramdisk?h=cm-11.0&id=34a1a7e4f68ca3f14fedad8c2f74a5ea2156a964

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -39,3 +39,10 @@ LOCAL_MODULE_CLASS	:= BOOT
 LOCAL_SRC_FILES		:= etc/ueventd.universal5430.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.sec.boot.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.sec.boot.sh
+include $(BUILD_PREBUILT)
