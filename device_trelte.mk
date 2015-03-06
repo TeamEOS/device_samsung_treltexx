167a5535e424f07898f44950f72032a299bca4b1
Christopher N. Hesse <raymanfx@gmail.com>
2015-03-07 00:10:09 +0100

    trelte: actually import Samsung ramdisk changes

--- device_trelte.mk
+++ device_trelte.mk
@@ -23,6 +23,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     fstab.universal5433 \
     init.carrier.rc \
+    init.samsung.rc \
     init.universal5433.rc \
     init.universal5433.usb.rc \
     init.universal5433.wifi.rc \
