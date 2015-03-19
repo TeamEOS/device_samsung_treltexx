b1a57d48c5ec9a6ee76d50b3493d9a78c3973f29
Christopher N. Hesse <raymanfx@gmail.com>
2015-03-19 23:42:28 +0100

    [HACK]: create output directories for inline kernel building

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,7 @@
 add_lunch_combo cm_trelte-eng
 add_lunch_combo cm_trelte-userdebug
+
+mkdir -p out/target/product/trelte/obj/KERNEL_OBJ/drivers/net/wireless/bcmdhd4358
+mkdir -p out/target/product/trelte/obj/KERNEL_OBJ/drivers/sensorhub/brcm/factory
+mkdir -p out/target/product/trelte/obj/KERNEL_OBJ/drivers/media/platform/exynos/fimc-is/sensor
+
