3f4a3ac5846b3b51961f8e41e851b27789f8eb45
Christopher N. Hesse <raymanfx@gmail.com>
2015-03-31 14:51:29 +0200

    [HACK]: trelte: create missing dir for lollipop kernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,4 +4,5 @@ add_lunch_combo cm_trelte-userdebug
 mkdir -p out/target/product/trelte/obj/KERNEL_OBJ/drivers/net/wireless/bcmdhd4358
 mkdir -p out/target/product/trelte/obj/KERNEL_OBJ/drivers/sensorhub/brcm/factory
 mkdir -p out/target/product/trelte/obj/KERNEL_OBJ/drivers/media/platform/exynos/fimc-is/sensor
+mkdir -p out/target/product/trelte/obj/KERNEL_OBJ/security/tima_uevent
 
