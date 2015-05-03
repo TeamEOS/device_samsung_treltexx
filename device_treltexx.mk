cabbb1e2a9e187ed984c133f6bee976bd40bdbe2
Christopher N. Hesse <raymanfx@gmail.com>
2015-05-04 00:10:23 +0200

    treltexx: Fake a Broadcom configuration for the adaption library to read
    Change-Id: I1cfeb7c21ba944cc9f5df5f9803342a914de03bd

--- device_treltexx.mk
+++ device_treltexx.mk
@@ -130,7 +130,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-sec-hal.conf:system/etc/libnfc-sec-hal.conf \
-    $(LOCAL_PATH)/configs/nfc/libnfc-sec.conf:system/etc/libnfc-sec.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-sec.conf:system/etc/libnfc-brcm.conf \
     $(LOCAL_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml
 
 PRODUCT_PACKAGES += \
