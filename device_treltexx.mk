3b3159f46993ac902323bd1d22e829ca5a7d9ab3
Christopher N. Hesse <raymanfx@gmail.com>
2015-05-18 14:16:52 +0200

    treltexx: Add vendor audio effects
    Change-Id: I189328e7843d10e217689955caec26c3eefa6919

--- device_treltexx.mk
+++ device_treltexx.mk
@@ -147,6 +147,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/configs/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
 PRODUCT_PACKAGES += \
