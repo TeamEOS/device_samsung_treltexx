SerenityS/android_device_samsung_slte

hal: Forgot to set the touchkey power

hal/power/power_hal.c
@@ -222,6 +222,7 @@ static void exynos5430_power_set_interactive(struct power_module *module, int on
                 on ? "1700000" : "800000");
 
     sysfs_write(exynos5430_pwr->touchscreen_power_path, on ? "1" : "0");
+    sysfs_write(exynos5430_pwr->touchkey_power_path, on ? "1" : "0");
     sysfs_write(exynos5430_pwr->gpio_keys_power_path, on ? "1" : "0");
 
     ALOGV("power_set_interactive: %d done\n", on);

TeamEOS/device_samsung_treltexx

Change-Id: Ibbcc9c7ae13641c12fb2f9fd7176063eb2118944

hal/power/power_hal.c
@@ -223,6 +223,7 @@ static void exynos5433_power_set_interactive(struct power_module *module, int on
      */
 
     sysfs_write(exynos5433_pwr->touchscreen_power_path, on ? "1" : "0");
+    sysfs_write(exynos5433_pwr->touchkey_power_path, on ? "1" : "0");
     sysfs_write(exynos5433_pwr->gpio_keys_power_path, on ? "1" : "0");
 
     ALOGV("power_set_interactive: %d done\n", on);