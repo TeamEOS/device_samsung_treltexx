f1a62c74e1043e2524cd9661f031b251d4ee7e3c
Christopher N. Hesse <raymanfx@gmail.com>
2015-06-24 12:19:43 +0200

    treltexx: Move inline kernel build hack to sane location
    Change-Id: I679f98dbf7ea3c569f49f59f847e7a0dd0714787

--- Android.mk
+++ Android.mk
@@ -17,5 +17,12 @@
 LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter treltexx, $(TARGET_DEVICE)),)
+
+$(shell mkdir -p out/target/product/treltexx/obj/KERNEL_OBJ/drivers/net/wireless/bcmdhd4358)
+$(shell mkdir -p out/target/product/treltexx/obj/KERNEL_OBJ/drivers/sensorhub/brcm/factory)
+$(shell mkdir -p out/target/product/treltexx/obj/KERNEL_OBJ/drivers/media/platform/exynos/fimc-is/sensor)
+$(shell mkdir -p out/target/product/treltexx/obj/KERNEL_OBJ/security/tima_uevent)
+
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
 endif
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,2 @@
 add_lunch_combo eos_treltexx-eng
 add_lunch_combo eos_treltexx-userdebug
-
-mkdir -p out/target/product/treltexx/obj/KERNEL_OBJ/drivers/net/wireless/bcmdhd4358
-mkdir -p out/target/product/treltexx/obj/KERNEL_OBJ/drivers/sensorhub/brcm/factory
-mkdir -p out/target/product/treltexx/obj/KERNEL_OBJ/drivers/media/platform/exynos/fimc-is/sensor
-mkdir -p out/target/product/treltexx/obj/KERNEL_OBJ/security/tima_uevent
-
