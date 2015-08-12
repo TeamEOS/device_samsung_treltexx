96fed3bbc646be2f0015a51d17c532331ea9a4bb
Christopher N. Hesse <raymanfx@gmail.com>
2015-08-12 13:10:26 +0200

    treltexx: Update healthd configuration for charger
    Change-Id: I1e261727f83c26a26e8e0e0e508f45a48dee5a4e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -146,7 +146,13 @@ BOARD_HAVE_SAMSUNG_WIFI := true
 
 # Charger/Healthd
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
-BOARD_BATTERY_DEVICE_NAME := battery
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGER_SHOW_PERCENTAGE := true
+RED_LED_PATH := "/sys/class/leds/led_r/brightness"
+GREEN_LED_PATH := "/sys/class/leds/led_g/brightness"
+BLUE_LED_PATH := "/sys/class/leds/led_b/brightness"
+BACKLIGHT_PATH := "/sys/class/backlight/panel/brightness"
+CHARGING_ENABLED_PATH := /sys/class/power_supply/battery/batt_lp_charging
 
 # RIL
 BOARD_VENDOR := samsung
