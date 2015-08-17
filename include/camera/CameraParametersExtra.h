ec4625ea66c6868f1021c38439114ae3e47d9d6f
Andreas Schneider <asn@cryptomilk.org>
2015-08-17 10:13:24 +0200

    include: Fix PIXEL_FORMAT_YUV420SP_NV21 string

--- include/camera/CameraParametersExtra.h
+++ include/camera/CameraParametersExtra.h
@@ -15,7 +15,7 @@
  */
 
 #define CAMERA_PARAMETERS_EXTRA_C \
-    const char CameraParameters::PIXEL_FORMAT_YUV420SP_NV21[] = "yuv420sp"; \
+    const char CameraParameters::PIXEL_FORMAT_YUV420SP_NV21[] = "nv21"; \
     const char CameraParameters::EFFECT_CARTOONIZE[] = "cartoonize"; \
     const char CameraParameters::EFFECT_POINT_RED_YELLOW[] = "point-red-yellow"; \
     const char CameraParameters::EFFECT_POINT_GREEN[] = "point-green"; \
