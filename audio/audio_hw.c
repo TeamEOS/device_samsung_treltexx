a02cc84a301efc5891e91df95de43e819698b42c
Christopher N. Hesse <raymanfx@gmail.com>
2015-08-29 18:38:35 +0200

    treltexx: Add narrow/wideband runtime switching to audio HAL
    The standard pcm voice call configs are reset to 8kHz.
    
    Change-Id: I7232dfec08e58e96422d4b1bb059fbdda2dc5d2a

--- audio/audio_hw.c
+++ audio/audio_hw.c
@@ -98,7 +98,7 @@ struct pcm_config pcm_config_in_low_latency = {
 
 struct pcm_config pcm_config_sco = {
     .channels = 1,
-    .rate = 16000,
+    .rate = 8000,
     .period_size = 128,
     .period_count = 2,
     .format = PCM_FORMAT_S16_LE,
@@ -114,7 +114,7 @@ struct pcm_config pcm_config_sco_wide = {
 
 struct pcm_config pcm_config_voice = {
     .channels = 2,
-    .rate = 16000,
+    .rate = 8000,
     .period_size = 1024,
     .period_count = 2,
     .format = PCM_FORMAT_S16_LE,
@@ -228,6 +228,12 @@ const struct string_to_enum out_channels_name_to_enum_table[] = {
     STRING_TO_ENUM(AUDIO_CHANNEL_OUT_5POINT1),
     STRING_TO_ENUM(AUDIO_CHANNEL_OUT_7POINT1),
 };
+
+/* Wideband call audio check */
+static bool wideband_disabled()
+{
+    return property_get_bool("audio_hal.disable_wideband", false);
+}
 
 /* Routing functions */
 
@@ -370,7 +376,10 @@ static void start_bt_sco(struct audio_device *adev)
 
     ALOGV("%s: Opening SCO PCMs", __func__);
 
-    sco_config = &pcm_config_sco;
+    if (!wideband_disabled())
+        sco_config = &pcm_config_sco_wide;
+    else
+        sco_config = &pcm_config_sco;
 
     adev->pcm_sco_rx = pcm_open(PCM_CARD, PCM_DEVICE_SCO, PCM_OUT,
             sco_config);
@@ -433,7 +442,10 @@ static int start_voice_call(struct audio_device *adev)
 
     ALOGV("%s: Opening voice PCMs", __func__);
 
-    voice_config = &pcm_config_voice;
+    if (!wideband_disabled())
+        voice_config = &pcm_config_voice_wide;
+    else
+        voice_config = &pcm_config_voice;
 
     /* Open modem PCM channels */
     adev->pcm_voice_rx = pcm_open(PCM_CARD, PCM_DEVICE_VOICE, PCM_OUT | PCM_MONOTONIC,
