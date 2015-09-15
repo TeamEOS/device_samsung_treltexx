07fbf01846bda0f33a307c13af70e38c35be1810
Christopher N. Hesse <raymanfx@gmail.com>
2015-09-15 18:37:40 +0200

    treltexx: Remove dependance on framework memory configs
    Change-Id: I86ab5bfd8c465a06ca59038d0d417dbfaa3f5759

--- device.mk
+++ device.mk
@@ -344,10 +344,36 @@ PRODUCT_PACKAGES += \
 ADDITIONAL_DEFAULT_PROPERTIES += \
     ro.debug_level=0x4948 \
     ro.secure=0
-    
 
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
+###########################################################
+### DALVIK/ART
+###########################################################
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=2m \
+    dalvik.vm.heapmaxfree=8m
+
+###########################################################
+### HWUI
+###########################################################
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.texture_cache_size=88 \
+    ro.hwui.layer_cache_size=58 \
+    ro.hwui.path_cache_size=32 \
+    ro.hwui.shape_cache_size=4 \
+    ro.hwui.gradient_cache_size=2 \
+    ro.hwui.drop_shadow_cache_size=8 \
+    ro.hwui.r_buffer_cache_size=8 \
+    ro.hwui.text_small_cache_width=2048 \
+    ro.hwui.text_small_cache_height=2048 \
+    ro.hwui.text_large_cache_width=4096 \
+    ro.hwui.text_large_cache_height=4096
+
 $(call inherit-product-if-exists, build/target/product/full.mk)
 
 # call the proprietary setup
